b932d9d0809e26778895e81e6de811c8a19ad7e2
Baranova Ekaterina <recrane@rambler.ru>
2023-01-08 14:07:43 +1000

    changed file

--- Script.sql
+++ Script.sql
@@ -5,11 +5,11 @@ CREATE TABLE IF NOT EXISTS Genre (
 
 CREATE TABLE IF NOT EXISTS Singers(
     id SERIAL PRIMARY KEY,
-    name_singer VARCHAR(50) NOT NULL,
-    album TEXT NOT NULL 
+    name_singer VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Genre_Singer(
+    id SERIAL,
     genre_id INTEGER REFERENCES Genre(id),
     singer_id INTEGER REFERENCES Singers(id),
     CONSTRAINT pk PRIMARY KEY (genre_id, singer_id)
@@ -18,12 +18,12 @@ CREATE TABLE IF NOT EXISTS Genre_Singer(
 CREATE TABLE IF NOT EXISTS Albums(
     id SERIAL PRIMARY KEY,
     name_album TEXT UNIQUE,
-    release_year INT NOT NULL,
-    name_singer VARCHAR(60) NOT NULL
+    release_year INT NOT NULL
 );
 
 
 CREATE TABLE IF NOT EXISTS Album_Singer(
+    id SERIAL,
     id_album INTEGER REFERENCES Albums(id),
     id_singer INTEGER REFERENCES Singers(id),
     CONSTRAINT pk_as PRIMARY KEY (id_album, id_singer)
@@ -32,18 +32,18 @@ CREATE TABLE IF NOT EXISTS Album_Singer(
 CREATE TABLE IF NOT EXISTS Tracks(
     id SERIAL PRIMARY KEY,
     name_track TEXT NOT NULL,
-    length_track TIME NOT NULL,
-    album TEXT REFERENCES Albums(name_album)
+    length_track INT NOT NULL,
+    album INT REFERENCES Albums(id)
 );
 
 CREATE TABLE IF NOT EXISTS Collections(
     id SERIAL PRIMARY KEY,
     name_collection TEXT NOT NULL,
-    release_year INT NOT NULL,
-    track TEXT NOT NULL
+    release_year INT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Track_Collection(
+    id SERIAL,
     id_track INTEGER REFERENCES Tracks(id),
     id_collection INTEGER REFERENCES Collections(id),
     CONSTRAINT pk_tc PRIMARY KEY (id_track, id_collection)
