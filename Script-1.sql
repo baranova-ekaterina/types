ab354d21727644a7435133a2e614eafbd2610bb7
Baranova Ekaterina <recrane@rambler.ru>
2023-01-08 14:08:40 +1000

    changed file script1

--- Script-1.sql
+++ Script-1.sql
@@ -8,51 +8,97 @@ VALUES ('поп-музыка'),
        ('иностранный рок'),
        ('поп-рок');
        
-INSERT INTO Singers(name_singer, album)
-VALUES ('Хабиб', 'Ягода малинка'),
-       ('Ляпис Тубецкой', 'Ты кинула'),
-       ('Shaman', 'До самого неба'),
-       ('Imagine Dragons', 'Evolve'), 
-       ('Gramatik', 'Synthopia Prime'),
-       ('Nico Bellovic', 'We Won'),
-       ('Timofey Beynarovich', 'Dream of Freedom'),
-       ('Lindsey Stirling', 'Shatter me'),
-       ('Evanescence', 'Fallen');
+INSERT INTO Singers(name_singer)
+VALUES ('Хабиб'),
+       ('Ляпис Тубецкой'),
+       ('Shaman'),
+       ('Imagine Dragons'), 
+       ('Gramatik'),
+       ('Nico Bellovic'),
+       ('Timofey Beynarovich'),
+       ('Lindsey Stirling'),
+       ('Evanescence');
       
- INSERT INTO Albums(name_album, release_year, name_singer)
- VALUES ('Ягода малинка', '2020', 'Хабиб'),
-        ('Evolve', '2017', 'Imagin Dragons'),
-        ('До самого неба', '2021', 'Shaman'),
-        ('Ты кинула', '1998', 'Ляпис Трубецкой'),
-        ('Synthopia Prime', '2016', 'Gramatik'),
-        ('We Won', '2022', 'Nico Bellovic'),
-        ('Dream of Freedom', '2022', 'Timofey Beynarovich'),
-        ('Shatter me', '2014', 'Lindsey Stirling'),
-        ('Fallen', '2003', 'Evanscence');
+ INSERT INTO Albums(name_album, release_year)
+ VALUES ('Ягода малинка', '2020'),
+        ('Evolve', '2017'),
+        ('До самого неба', '2021'),
+        ('Ты кинула', '1998'),
+        ('Synthopia Prime', '2016'),
+        ('We Won', '2022'),
+        ('Dream of Freedom', '2022'),
+        ('Shatter me', '2014'),
+        ('Fallen', '2003');
        
 INSERT INTO Tracks(name_track, length_track, album)
-VALUES ('Грустинка', '2:26', 'Ягода малинка'),
-       ('Ягода малинка', '2:27', 'Ягода малинка'),
-       ('Thunder', '3:07', 'Evolve'),
-       ('Believer', '3:24', 'Evolve'),
-       ('Whatever it takes', '3:21', 'Evolve'),
-       ('До самого неба', '3:00', 'До самого неба'),
-       ('В платье белом', '4:58', 'Ты кинула'),
-       ('Пастушок', '4:38', 'Ты кинула'),
-       ('Party or Somerthing', '1:31', 'Synthopia Prime'),
-       ('Just a Girl', '1:54', 'We Won'),
-       ('Dream of Freedom', '2:54', 'Dream of Freedom'),
-       ('Shatter me', '4:40', 'Shatter me'),
-       ('Bring me to my life', '3:57', 'Fallen'),
-       ('Going Under', '3:34', 'Fallen'),
-       ('John the man', '1:54', 'We Won');
+VALUES ('Грустинка', '146', '1'),
+       ('Ягода малинка', '147', '1'),
+       ('Thunder', '187', '2'),
+       ('Believer', '204', '2'),
+       ('Whatever it takes', '201', '2'),
+       ('До самого неба', '180', '3'),
+       ('В платье белом', '258', '4'),
+       ('Пастушок', '288', '4'),
+       ('Party or Somerthing', '91', '5'),
+       ('Just a Girl', '114', '6'),
+       ('Dream of Freedom', '174', '7'),
+       ('Shatter me', '280', '8'),
+       ('Bring me to my life', '237', '9'),
+       ('Going Under', '214', '9'),
+       ('John the man', '114', '6');
        
-INSERT INTO Collections(name_collection, release_year, track)
-VALUES ('Русский хит 2020-2021', '2021', 'Грустинка'),
-       ('Top greatest hits', '2022', 'Thunder'),
-       ('Топ 25 песен Imagin Dragons', '2019', 'Whatever it takes'),
-       ('Дискотека 90-х', '2010', 'В платье белом'),
-       ('Странная музыка 2022', '2022', 'Just a Girl'),
-       ('Современная классика', '2017', 'Shatter me'),
-       ('Хиты 2003 года', '2004', 'Bring me to my life'),
-       ('Союз 32', '2016', 'Bring me to my life');
+INSERT INTO Collections(name_collection, release_year)
+VALUES ('Русский хит 2020-2021', '2021'),
+       ('Top greatest hits', '2022'),
+       ('Топ 25 песен Imagin Dragons', '2019'),
+       ('Дискотека 90-х', '2010'),
+       ('Странная музыка 2022', '2022'),
+       ('Современная классика', '2017'),
+       ('Хиты 2003 года', '2004'),
+       ('Союз 32', '2016');
+       
+INSERT INTO Album_Singer(id_album, id_singer)
+VALUES ('1', '1'),
+       ('2', '4'),
+       ('3', '3'),
+       ('3', '1'),
+       ('4', '2'),
+       ('5', '5'),
+       ('6', '6'),
+       ('7', '7'),
+       ('7', '8'),
+       ('8', '8'),
+       ('9', '9');
+      
+INSERT INTO Genre_Singer(genre_id, singer_id)
+VALUES ('1', '1'),
+       ('1', '3'),
+       ('2', '2'),
+       ('3', '5'),
+       ('4', '6'),
+       ('5', '7'),
+       ('6', '8'),
+       ('7', '9'),
+       ('8', '4');
+       
+INSERT INTO Track_Collection (id_track, id_collection)
+VALUES ('1', '1'),
+       ('2', '1'),
+       ('3', '2'),
+       ('4', '3'),
+       ('5', '3'),
+       ('6', '1'),
+       ('7', '4'),
+       ('8', '4'),
+       ('9', '6'),
+       ('10', '5'),
+       ('11', '6'),
+       ('12', '6'),
+       ('13', '8'),
+       ('14', '7'),
+       ('15', '5');
+       
+       
+       
+       
+       
